94ff382ac96e52c093353f15725a039f27ea0083
Granit <65214984+mentos1337@users.noreply.github.com>
2022-08-28 01:42:23 +0200

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,14 @@
+def validate_name(name):
+
+    if not name.isalpha():
+        return "Bare lov med bokstaver, prøv igjen"
+    else:
+        if not 2<=len(name)<=12:
+            return "Navnet må inneholde minst 2 bokstaver og maks 12"
+        else: 
+            return "Hello " + name
+#Input name, output message
+
+print("hva heter du?")
+name = input()
+print(validate_name(name))
