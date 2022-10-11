6bb5165c3e6f16643dcd06b8638db2447db0d44f
Granit <65214984+mentos1337@users.noreply.github.com>
2022-10-11 23:17:07 +0200

    w

--- oblig4_Granit-Salihu/programmeringsoppgave1.py
+++ oblig4_Granit-Salihu/programmeringsoppgave1.py
@@ -3,6 +3,7 @@ class filmer:
         self.filmtittel = filmtittel
         self.utgivelsesår = utgivelsesår
         self.score = score
+        print(f"{filmtittel} was released in {utgivelsesår} and currently has a score of {score}")
     def printer(self):
         return (f"{self.filmtittel} was released in {self.utgivelsesår} and currently has a score of {self.score}")
 
@@ -10,6 +11,7 @@ film1 = filmer("Inception", 2010, 8.8)
 film2 = filmer("The Martian", 2015, 8.0)
 film3 = filmer("Joker", 2019, 8.4)
 
+print()
 print(film1.printer())
 print(film2.printer())
 print(film3.printer())
