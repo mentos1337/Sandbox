0966c3f93f6697927cc627800be6c5582304659b
Granit <65214984+mentos1337@users.noreply.github.com>
2022-10-13 11:41:44 +0200

    eaa

--- Øvingsoppgaver/programmeringstrening/programpraktisk/hangman.py
+++ Øvingsoppgaver/programmeringstrening/programpraktisk/hangman.py
@@ -1,25 +1,39 @@
-from operator import index, indexOf
 import random
-from tkinter import INSERT
 
+#liste med ord
 words = ["Apekatt", "Sushi", "Kebab", "Armadillo"]
 
+#tar et random ord fra lista
 wordToGuess = words[random.randrange(0, len(words))]
+
+#lager liste av ordet
 wordToGuessList = list(wordToGuess)
-print(wordToGuessList)
+
+#lager string (_) ut av ordet som ble valgt ut
 hint = ["_"] * len(wordToGuess)
-print(hint)
-print()
 
 lives = 6
 
-while "_" in hint and lives >= 1:
+while "_" in hint and lives > 0:
     print("".join(hint).capitalize())
     print("liv igjen", lives)
-    guessLetter = input("Guess a letter: ") 
-    if guessLetter.lower() in wordToGuessList:
-        for index in range(len(wordToGuessList)):
-            if guessLetter.lower() == wordToGuessList[index].lower():
-                hint[index] = guessLetter
-    else:
-        lives -= 1
+    guessLetter = input("Guess a letter: ")
+    if not guessLetter.isalpha():
+        print("You cannt guess anything other than letters")
+        continue
+
+    for character in guessLetter:
+        if guessLetter.lower() in wordToGuess.lower():
+            for index in range(len(wordToGuessList)):
+                if guessLetter.lower() == wordToGuessList[index].lower():
+                    hint[index] = guessLetter
+        else:
+            lives -= 1
+
+    if "_" not in hint:
+        print(f"Du vant med {lives} liv igjen")
+        print(f"du gjettet ordet riktig {wordToGuess}")
+    elif lives == 0:
+        print(f"Du tapte med {lives} liv igjen")
+        print(f"Du gjettet: {''.join(hint).capitalize()}")
+        print(f"du gjettet ordet feil ordet var: {wordToGuess}")
