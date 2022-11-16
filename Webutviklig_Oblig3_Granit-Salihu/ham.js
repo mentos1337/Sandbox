f25140f313aad832fbfbd2be9d145a54848a4645
Granit <65214984+mentos1337@users.noreply.github.com>
2022-11-16 15:12:04 +0100

    eee

--- Webutviklig_Oblig3_Granit-Salihu/ham.js
+++ Webutviklig_Oblig3_Granit-Salihu/ham.js
@@ -3,8 +3,10 @@ const menu = document.getElementById("main-menu");
 const hamMenu = () =>{
     if(menu.style.display == "block"){
         menu.style.display ="none";
+        console.log("Hei det skjer");
     }
     else{
         menu.style.display = "block";
+        console.log("hei");
     };
 }
