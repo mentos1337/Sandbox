99dcb266c43e01bb9e0d8c00cc8160cef4fd5728
Granit <65214984+mentos1337@users.noreply.github.com>
2022-12-02 04:45:23 +0100

    efefe

---
+++ Webutviklig_4_Granit-Salihu/share.js
@@ -0,0 +1,10 @@
+const share = document.getElementById("share-menu");
+
+const shareMenu = () =>{
+    if(share.style.display == "block"){
+        share.style.display ="none";
+    }
+    else{
+        share.style.display = "block";
+    };
+}
