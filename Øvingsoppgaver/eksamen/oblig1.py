1ca4022859071a2baf2448201ffcd5f1453f9927
Granit <65214984+mentos1337@users.noreply.github.com>
2022-12-07 10:35:37 +0100

    mxjxj

---
+++ Øvingsoppgaver/eksamen/oblig1.py
@@ -0,0 +1,42 @@
+print("hello world")
+print()
+print()
+fornavn = "Granit"
+etternavn = "Salihu"
+alder = 27
+
+print(f"Hei jeg heter {fornavn} {etternavn} og er {alder} år gammel")
+print()
+print()
+
+tall_1 = int(input("Velg det første tallet: "))
+tall_2 = int(input("Velg det andre tallet: "))
+print(tall_1 * tall_2)
+print(tall_1 / tall_2)
+print(tall_1 + tall_2)
+print(tall_1 - tall_2)
+print(tall_1 % tall_2)
+print(tall_1 ** tall_2)
+print(tall_1 // tall_2)
+print()
+print()
+
+a = 6
+b = 3
+c = 2
+
+print(a + b * c)
+print((a+b)*c)
+print(a/b/c)
+print(a/(b/c))
+print()
+print()
+
+person1 = 5
+person2 = 9
+person3 = 2.5
+person4 = 21
+person5 = 0
+
+gjennomsnitt = int((person1 + person2 + person3 + person4 + person5)/5)
+print(gjennomsnitt)
